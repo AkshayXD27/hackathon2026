b7e5840b9a1fefb52dbfb2affb6ec8cea7bb6e83
AkshayXD27 <lostamongstars27@gmail.com>
2026-04-16 14:28:23 +0530

    updated and added firebase auth / login system

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUbpV2ar-GS66Ikp1PJR0quAPJdH2Byow",
+  authDomain: "hackathon-20261.firebaseapp.com",
+  projectId: "hackathon-20261",
+  storageBucket: "hackathon-20261.firebasestorage.app",
+  messagingSenderId: "304419961290",
+  appId: "1:304419961290:web:ffc874b1127ed94f757733"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
