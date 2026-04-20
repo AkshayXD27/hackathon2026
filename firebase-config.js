d9d8201f403a5d4ae73131cc1eeafbab76ef9b85
Akshay XD <lostamongstars27@gmail.com>
2026-04-20 09:26:54 +0530

    Initialize Firebase with configuration and exports
    Added Firebase configuration and initialization.

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,16 @@
-// enter ur firebase config
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUbpV2ar-GS66Ikp1PJR0quAPJdH2Byow", // pretty much safe to expose
+  authDomain: "hackathon-20261.firebaseapp.com",
+  projectId: "hackathon-20261",
+  storageBucket: "hackathon-20261.firebasestorage.app",
+  messagingSenderId: "304419961290",
+  appId: "1:304419961290:web:ffc874b1127ed94f757733"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
